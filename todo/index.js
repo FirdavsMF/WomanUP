4cd6987f0e217877a140ee62ee327c56af20710a
Firdavs <firdavs.2001.fed@gmail.com>
2022-11-23 09:44:04 +0300

    wrote a router

---
+++ todo/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const router = express.Router()
+const TODO require('../db/models/todo')
+
+router.post('/', (req, res) => {
+	const {username, value} = req.body
+	const newTODO = new TODO({
+		'username': username,
+		'value': value
+	})
+	newTODO.save((err, saveTODO) => {
+		if (err) return res.json(err)
+		return res.json(saveTODO)
+	})
+})
+
+router.get('/', (req, res, next) => {
+	TODO.find()
+	.then(data => res.json(data))
+	.catch(next)
+});
+
+router.delete('/:id', (req, res, next) => {
+	const {value} = req.body
+	TODO.findOneAndUpdate({"_id": req.params.id}, {$set: {value: value, updated: Data.new()}})
+	.then(data => res.json(data))
+	.catch(next)
+})
+
+module.exports = require
+
